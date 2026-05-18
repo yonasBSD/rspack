77524ede3055ec13ee3119112889dd81b7122c11
Cong-Cong Pan <dacongsama@live.com>
2026-05-18 16:20:16 +0800

    fix(rsc): group client chunks by server-entry ownership (#13880)

--- tests/rspack-test/configCases/rsc-plugin/client-chunk-grouping/src/Root.js
+++ tests/rspack-test/configCases/rsc-plugin/client-chunk-grouping/src/Root.js
@@ -1,15 +1,17 @@
-import { RootClient } from './clients/RootClient';
+import { RootOnlyA } from './clients/RootOnlyA';
+import { RootOnlyB } from './clients/RootOnlyB';
 import { SharedRootAndPage } from './clients/SharedRootAndPage';
 import { PageOne } from './pages/PageOne';
 import { PageTwo } from './pages/PageTwo';
 
-export const App = () => {
+export const Root = async () => {
   return (
-    <>
-      <RootClient />
+    <main>
+      <RootOnlyA />
+      <RootOnlyB />
       <SharedRootAndPage />
       <PageOne />
       <PageTwo />
-    </>
+    </main>
   );
 };
